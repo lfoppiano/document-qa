66a029169ffed07db840a75ae4b8bc9d33df34f1
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2023-10-14 13:10:06 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='document-qa-engine',
+    version='0.1',
+    url='https://github.com/lfoppiano/document-qa',
+    license='Apache 2.0',
+    author='lfoppiano',
+    author_email='lucanoro@duck.com',
+    description=''
+)
