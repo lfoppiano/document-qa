93bc8ec1f85fd92796b4d9c3e822936512231cc4
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2024-02-08 21:07:17 +0900

    fix paths

--- tests/test_grobid_processors.py
+++ tests/test_grobid_processors.py
@@ -1,9 +1,12 @@
+import os
+
 from bs4 import BeautifulSoup
 from document_qa.grobid_processors import get_xml_nodes_body, get_xml_nodes_figures, get_xml_nodes_header
+from tests.resources import TEST_DATA_PATH
 
 
 def test_get_xml_nodes_body_paragraphs():
-    with open("resources/2312.07559.paragraphs.tei.xml", 'r') as fo:
+    with open(os.path.join(TEST_DATA_PATH, "2312.07559.paragraphs.tei.xml"), 'r') as fo:
         soup = BeautifulSoup(fo, 'xml')
 
     nodes = get_xml_nodes_body(soup, use_paragraphs=True)
@@ -12,7 +15,7 @@ def test_get_xml_nodes_body_paragraphs():
 
 
 def test_get_xml_nodes_body_sentences():
-    with open("resources/2312.07559.sentences.tei.xml", 'r') as fo:
+    with open(os.path.join(TEST_DATA_PATH, "2312.07559.sentences.tei.xml"), 'r') as fo:
         soup = BeautifulSoup(fo, 'xml')
 
     children = get_xml_nodes_body(soup, use_paragraphs=False)
@@ -21,7 +24,7 @@ def test_get_xml_nodes_body_sentences():
 
 
 def test_get_xml_nodes_figures():
-    with open("resources/2312.07559.paragraphs.tei.xml", 'r') as fo:
+    with open(os.path.join(TEST_DATA_PATH, "2312.07559.paragraphs.tei.xml"), 'r') as fo:
         soup = BeautifulSoup(fo, 'xml')
 
     children = get_xml_nodes_figures(soup)
@@ -30,15 +33,16 @@ def test_get_xml_nodes_figures():
 
 
 def test_get_xml_nodes_header_paragraphs():
-    with open("resources/2312.07559.paragraphs.tei.xml", 'r') as fo:
+    with open(os.path.join(TEST_DATA_PATH, "2312.07559.paragraphs.tei.xml"), 'r') as fo:
         soup = BeautifulSoup(fo, 'xml')
 
     children = get_xml_nodes_header(soup)
 
     assert len(children) == 8
 
+
 def test_get_xml_nodes_header_sentences():
-    with open("resources/2312.07559.sentences.tei.xml", 'r') as fo:
+    with open(os.path.join(TEST_DATA_PATH, "2312.07559.sentences.tei.xml"), 'r') as fo:
         soup = BeautifulSoup(fo, 'xml')
 
     children = get_xml_nodes_header(soup, use_paragraphs=False)
